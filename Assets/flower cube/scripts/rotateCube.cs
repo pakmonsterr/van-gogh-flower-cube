8bab39fad0dcbede78ec76e07df779d81c4bdf4c
Hanna Pak <hannacarlie@gmail.com>
2023-07-24 18:03:47 +0900

    have continous rotation from release vector

--- Assets/flower cube/scripts/rotateCube.cs
+++ Assets/flower cube/scripts/rotateCube.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class rotateCube : MonoBehaviour
 {
     private bool first_touch, release, decaying;
-    private Vector3 start_pos;
-    private Quaternion current_rot, quat;
+    private Vector3 start_pos, move_start, move_delta;
+    private Quaternion current_rot, quat, drag_quat;
 
     public GameObject R_controller;
+
+    public TMP_Text debug_text_1;
+    public TMP_Text debug_text_2;
+    public float timer = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         first_touch = true;
         release = false;
+        decaying = false;
     }
 
     // Update is called once per frame
@@ -24,6 +30,7 @@ public class rotateCube : MonoBehaviour
         {
             if (first_touch)
             {
+                // when cube first grabbed, get vector from starting position to cube center & cube rotation
                 start_pos = Vector3.Normalize(2 * R_controller.transform.position - gameObject.transform.position);
                 current_rot = gameObject.transform.rotation;
                 first_touch = false;
@@ -31,27 +38,67 @@ public class rotateCube : MonoBehaviour
             }
             else
             {
+                StartCoroutine(getMovementVector());
+                
+                // when controller dragged, get vector from closest point on cube, make quaternion from starting vector to current pos vector
                 Vector3 closest_point = Vector3.Normalize(2 * R_controller.transform.position - gameObject.transform.position);
                 quat = Quaternion.FromToRotation(start_pos, closest_point);
+
+                // apply quaternion to cube (quat * quat to double movement speed)
                 gameObject.transform.rotation = quat * quat * current_rot;
             }
         }
         else
         {
-            if (release && decaying)
+            if (!decaying)
             {
-                first_touch = true;
-                release = false;
-                StartCoroutine(decayMotion());
+                if (release)
+                {
+                    StartCoroutine(onRelease());
+                }
+                else
+                {
+                    // if cube released & done decaying, idle rotate 
+                    gameObject.transform.Rotate(new Vector3(15, 15, 15) * Time.deltaTime);
+                }
             }
-            
-            gameObject.transform.Rotate(new Vector3(15, 15, 15) * Time.deltaTime);
+            else 
+            {
+                debug_text_2.text = $"{timer}";
+                debug_text_1.text = $"{move_delta}";
+                    
+                timer += Time.deltaTime;
+
+                if (timer < 2.0f)
+                {
+                    gameObject.transform.rotation = drag_quat * gameObject.transform.rotation;
+                }
+                else
+                {
+                    decaying = false;
+                }
+            }
         }
     }
 
-    IEnumerator decayMotion()
+    IEnumerator getMovementVector()
     {
-        
-        yield return new WaitForSeconds(0.0f);
+        move_start = R_controller.transform.position - gameObject.transform.position;
+
+        yield return new WaitForSeconds(0.05f);
+
+        drag_quat = Quaternion.FromToRotation(move_start, R_controller.transform.position - gameObject.transform.position);
+    }
+
+    IEnumerator onRelease()
+    {
+        // upon release of cube, start decaying motion
+        first_touch = true;
+        release = false;
+        decaying = true;
+
+        timer = 0;
+
+        yield return new WaitForSeconds(0.00f);
     }
 }
